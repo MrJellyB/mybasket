fced5ab04ecdebc9e90fb085ee8a1b1e4f436d66
liorstrongxd <liorstrongxd@BMW23>
2017-12-22 14:24:04 +0200

    יצירת השרת

--- MyBasket/MyBasket.Server/server.js
+++ MyBasket/MyBasket.Server/server.js
@@ -1,17 +1,49 @@
 'use strict';
 var express = require("express");
 var http = require("http");
+var mongodb = require('./node_modules/mongodb');
+var MongoClient = mongodb.MongoClient;
 
 var app = express();
 
 var database = {};
 
+var users = [];
+
+var userCollections = {};
+
 var url = 'mongodb://localhost:27017';
 
-//app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/src"));
 
 var server = http.createServer(app);
 
 server.listen(8080, null, null, function () {
     console.log("server running at http://8080");
 });
+
+
+// set up the db
+var setupToDb = function(url) {
+    MongoClient.connect(url, function (err, client) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        }
+
+        database = client.db('test');
+
+        userCollections = database.collection('users');
+    })
+}
+
+// create the database
+setupToDb(url);
+
+
+app.get('/getUsers', function(req, res) {
+    var users = [];
+    userCollections.find({}).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+})
