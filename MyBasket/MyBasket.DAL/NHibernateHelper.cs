e0af5dea6da7dd56499c8eaacebf7e258ffb48cd
liormiz <liorstrongxd>
2017-12-26 19:47:51 +0200

    הוספת דברים שקשורים לnhibernate

---
+++ MyBasket/MyBasket.DAL/NHibernateHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MyBasket.DAL.Entities;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using NHibernate.Tool.hbm2ddl;
+
+
+namespace MyBasket.DAL
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory { get; set; }
+
+        private static void OpenSession()
+        {
+            Configuration configuration = new Configuration();
+            configuration.AddAssembly(Assembly.GetCallingAssembly());
+            SessionFactory = configuration.BuildSessionFactory();
+        }
+
+        public static ISession GetCurrentSession()
+        {
+            if (SessionFactory == null)
+                NHibernateHelper.OpenSession();
+
+            return SessionFactory.OpenSession();
+        }
+
+        public static void CloseSessionFactory()
+        {
+            if (SessionFactory != null)
+                SessionFactory.Close();
+        }
+    }
+}
---
+++ MyBasket/MyBasket.DAL/checknHibernate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBasket.DAL.Entities;
+using NHibernate;
+
+namespace MyBasket.DAL
+{
+    public class checknHibernate
+    {
+        static void Main(string[] args)
+        {
+            string nId = "1";
+            ISession session = NHibernateHelper.GetCurrentSession();
+            IQuery query = session.CreateQuery("FROM FinalProject.dbo.BS_PRODUCT_TB WHERE Id = 1");
+            query.SetString("name", nId);
+            IList<Product> acc = query.List<Product>();
+            session.Close();
+            if (acc.Count == 0)
+                Console.WriteLine("Cannot find specified user");
+            else
+                Console.WriteLine("Found " + acc[0].Id);
+
+            Console.ReadKey();
+        }
+    }
+}
