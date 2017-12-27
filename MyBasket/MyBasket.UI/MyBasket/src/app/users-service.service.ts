4a84bd1de3675821d8a448cdd61d336d27f2af09
Noam Shvecher <shvachernoam@cs.colman.ac.il>
2017-12-27 20:02:41 +0200

    added getUsers to user service

--- MyBasket/MyBasket.UI/MyBasket/src/app/users-service.service.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/users-service.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { User } from './user-login-component/User';
 import * as dummyjson from "dummy-json";
+import { USERS } from './mock-users'
 
 @Injectable()
 export class UsersServiceService {
 
   getUsers(): User[] {
-    return dummyjson.parse('./data/mock.data');
+    return USERS;
   }
 
 }
