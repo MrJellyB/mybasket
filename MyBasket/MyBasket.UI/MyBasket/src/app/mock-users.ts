50c52ac731d10e232cd013a6242e81c5a4265b02
Shvacher Noam <shvachernoam@gmail.com>
2017-12-27 20:41:23 +0200

    fixed fs npm package not found

--- MyBasket/MyBasket.UI/MyBasket/src/app/mock-users.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/mock-users.ts
@@ -1,6 +1,6 @@
 import { User } from './user-login-component/User';
 import dummyjson = require("dummy-json");
-import fs = require('fs');
+import fs = require("fs");
 
 
 export const USERS: User[] = dummyjson.parse(fs.readFileSync('./data/mock.data', {encoding: 'utf8'}));
