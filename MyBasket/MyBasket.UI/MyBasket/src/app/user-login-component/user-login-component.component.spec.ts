fe779a85cdc3eb308efafd04be4a5c09d196ea53
Noam <shvachernoam@gmail.com>
2017-12-30 13:22:49 +0200

    changed 'user-login-component.component' to 'user-login.component'

--- MyBasket/MyBasket.UI/MyBasket/src/app/user-login-component/user-login-component.component.spec.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/user-login-component/user-login-component.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserLoginComponent } from './user-login-component.component';
+import { UserLoginComponent } from './user-login.component';
 
 describe('UserLoginComponent', () => {
   let component: UserLoginComponent;
--- MyBasket/MyBasket.UI/MyBasket/src/app/user-login-component/user-login.component.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/user-login-component/user-login.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { UsersServiceService } from '../users-service.service';
 
 @Component({
-  selector: 'app-user-login-component',
-  templateUrl: './user-login-component.component.html',
-  styleUrls: ['./user-login-component.component.css']
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
 
   errorConnecting:boolean;
 
-  constructor(private userService: UsersServiceService) { 
+  constructor(private userService: UsersServiceService) {
     this.errorConnecting = false;
   }
 
@@ -22,7 +22,7 @@ export class UserLoginComponent implements OnInit {
     this.userService.login(email, password).then(
       (result) => {
         // TODO: add here a router redirection to main page with the user credentials
-        // (result) ? 
+        // (result) ?
       },
     (err) => {
       // Write an error through the alerting service
