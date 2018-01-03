2272fde481ff594686b766245fbc2a5b105232bb
Noam <shvachernoam@gmail.com>
2018-01-03 10:39:28 +0200

    created basic main navbar

--- MyBasket/MyBasket.UI/MyBasket/src/app/shared/app-routing/app-routing.module.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/shared/app-routing/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { UserLoginComponent } from '../../modules/login/user-login-component/user-login.component';
 import { RegisterUserComponent } from '../../modules/login/register-user/register-user.component';
 import { SideNavBarComponent } from '../component/side-nav-bar/side-nav-bar.component';
+import { MainNavBarComponent } from '../component/main-nav-bar/main-nav-bar.component';
 
 export const routes:Routes = [
   { path: 'login', component: UserLoginComponent },
   { path: 'register', component: RegisterUserComponent },
-  { path: 'left-nav-bar', component: SideNavBarComponent }
+  { path: 'left-nav-bar', component: SideNavBarComponent },
+  { path: 'top-nav-bar', component: MainNavBarComponent }
 ];
 
 @NgModule({
