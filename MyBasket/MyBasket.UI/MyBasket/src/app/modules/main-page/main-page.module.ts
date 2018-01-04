48fbd247a740f05f9cdac1d97342a6e2f69dfd37
OrenHayak <orenheickk@gmail.com>
2018-01-04 20:31:08 +0200

    Create search-products

--- MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/main-page.module.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/main-page.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CategoryService } from './category.service';
 import { ProductDetailsComponent } from './product-details/product-details.component'
-import { SharedModule } from '../../shared/shared.module'
+import { SharedModule } from '../../shared/shared.module';
+import { SearchProductsComponent } from './search-products/search-products.component'
 
 @NgModule({
-  declarations: [MainPageComponent, ProductDetailsComponent],
+  declarations: [MainPageComponent, ProductDetailsComponent, SearchProductsComponent],
   imports: [
     CommonModule, SharedModule
   ],
