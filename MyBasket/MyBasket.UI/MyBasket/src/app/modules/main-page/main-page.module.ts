ed154b76d5c6aa609cb330f406cf953c25010808
OrenHayak <orenheickk@gmail.com>
2018-01-04 16:57:44 +0200

    Create title-bar component

--- MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/main-page.module.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/main-page.module.ts
@@ -4,9 +4,10 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { CategoryService } from './category.service';
 import { SharedModule } from '../../shared/shared.module'
 import { HttpService } from '../../shared/services/httpService/http.service';
+import { TitleBarComponent } from '../../shared/component/title-bar/title-bar/title-bar.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, TitleBarComponent],
   imports: [
     CommonModule, SharedModule
   ],
