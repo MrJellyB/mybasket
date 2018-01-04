c045768322d95e2f9e106ae928473e14411e9a20
liorstrongxd <liorstrongxd@BMW23>
2018-01-04 16:39:53 +0200

    ה component של בניית פרופיל אישי

--- MyBasket/MyBasket.UI/MyBasket/src/app/modules/login/login.module.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/modules/login/login.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms'
 import { UserLoginComponent } from './user-login-component/user-login.component';
 import { UsersServiceService } from './users-service.service';
 import { RegisterUserComponent } from './register-user/register-user.component';
+import { ProfileBuilderComponent } from './profile-builder/profile-builder.component';
 
 @NgModule({
-  declarations: [UserLoginComponent, RegisterUserComponent],
+  declarations: [UserLoginComponent, RegisterUserComponent, ProfileBuilderComponent],
   imports: [
     CommonModule,
     FormsModule
   ],
   providers: [UsersServiceService],
   bootstrap: [],
-  exports: [UserLoginComponent, RegisterUserComponent]
+  exports: [UserLoginComponent, RegisterUserComponent, ProfileBuilderComponent]
 })
 export class LoginModule { }
