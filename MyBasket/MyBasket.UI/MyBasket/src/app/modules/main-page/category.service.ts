a8096f5399077f4026cc60f321b3f3c66d746560
Noam <shvachernoam@gmail.com>
2018-01-04 16:38:00 +0200

    created empty category

--- MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/category.service.ts
+++ MyBasket/MyBasket.UI/MyBasket/src/app/modules/main-page/category.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import {} from './app'
+import { HttpService } from '../../shared/services/httpService/http.service';
 
 @Injectable()
 export class CategoryService {
 
-  constructor() { }
+  constructor(private httpService:HttpService) { }
 
 }
