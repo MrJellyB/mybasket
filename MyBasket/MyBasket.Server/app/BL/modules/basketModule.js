369f858834e149bd2a07f7860290e10d47308784
Noam <shvachernoam@gmail.com>
2018-01-04 19:23:54 +0200

    fixed categories for navbar

--- MyBasket/MyBasket.Server/app/BL/modules/basketModule.js
+++ MyBasket/MyBasket.Server/app/BL/modules/basketModule.js
@@ -29,5 +29,5 @@ exports.register = function (req, res) {
 }
 
 exports.getCategories = function(req, res) {
-    res.send({});
+    dbUtils.getCategories(function(err, data) { res.send(data); });
 }
